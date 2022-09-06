beb0c43d558896980b35a7fbf374722a9048c78a
TyfangXV <9fangdev@gmail.com>
2022-09-06 19:38:19 +0400

    New UI changes

---
+++ src/components/nav.svelte
@@ -0,0 +1,161 @@
+<script>
+      import { browser } from '$app/environment';
+    let is_Nav_M_Ctn_Open = false;
+
+    const HandleBurgerClick = () => {
+        if(browser)
+        {
+            const Nav_M_Ctn = document.getElementById("Nav_M_Ctn");
+            if(is_Nav_M_Ctn_Open)
+            {
+                is_Nav_M_Ctn_Open = false;
+                Nav_M_Ctn?.style.setProperty("display", "none")
+            }else{
+                is_Nav_M_Ctn_Open = true;
+                Nav_M_Ctn?.style.setProperty("display", "flex")
+            }            
+        }
+
+    }
+</script>
+
+<div class="nav">
+    <div class="logo">
+        <img src="/logo.png" alt="logo" class="logo-img"/>
+        <h1>DCC</h1>
+    </div>
+
+    <div class="ALink_M">
+        <img src="/menu-outlined.svg" alt="Menu_icon" class="Icon_menu" on:click={() => HandleBurgerClick()}/>
+        <div class="Pop_Ctn" id="Nav_M_Ctn">
+            <a href="/">Home</a>
+            <a href="/">Social</a>
+            <a href="/">Event</a>
+            <a href="/">Team</a>
+            <a href="/">Contact</a>
+        </div>
+    </div>
+
+    <div class="Alinks">
+        <a href="/">Home</a>
+        <a href="/">Social</a>
+        <a href="/">Event</a>
+        <a href="/">Team</a>
+        <a href="/">Contact</a>
+    </div>
+</div>
+
+<style>
+
+    .ALink_M{
+        margin-right: 5%;
+        display: none;        
+    }
+
+    .Icon_menu{
+        width: 35px;
+        height: 35px;
+        cursor: pointer;
+    }
+
+
+    .Pop_Ctn{
+        position: absolute;
+        width: 45vw;
+        height: 65vh;
+        display: flex;
+        flex-direction: column;
+        background-color: white;
+        align-items: center;
+        bottom: 25%;
+        left: 55%;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
+        border-top-left-radius: 10px;
+        z-index: 12;
+        transition: all 1s ease-in;
+        display: none;
+    }
+
+    .Pop_Ctn a{
+        padding-bottom: 10%;
+        padding-top:10%;
+        font-size: x-large;
+        text-decoration: none;
+        font-family: var(--font_M);
+        color: black;
+    }
+
+    .Pop_Ctn a:hover{
+        border: none;
+        outline: none;
+        color: grey;
+        transition: all 0.5s ease-in-out;
+    }
+
+    .Alinks{
+        display: flex;
+        flex-direction: row;
+        margin-right: 18%;
+    }
+
+    .Alinks a{
+        margin-left: 15%;
+        font-size: x-large;
+        text-decoration: none;
+        color: white;
+        font-family: var(--font_M);
+        font-weight: 400;
+    }
+
+
+    .Alinks a:hover {
+        border: none;
+        outline: none;
+        color: black;
+        transition: all 0.5s ease-in-out;
+    }
+
+    .logo-img{
+        width: 70px;
+        height: 70px;
+    }
+
+    .logo{
+        margin-left: 2%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .logo h1{
+        margin-left: 10px;
+        font-family: var(--font_M);
+        color: rgb(88, 88, 88);
+    }
+
+    .nav{
+        display: flex;
+        justify-content: space-between;
+        height: 80px;
+        align-items: center;
+        width: 100vw;
+        background: transparent;
+        box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+    }
+
+
+    @media only screen and (max-width: 600px) {
+        .Alinks{
+            display: none;
+        }
+        .ALink_M{
+            display: flex;
+        }
+
+        .nav{
+            background-color: white;
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,138 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import Buttons from "../components/buttons.svelte";
+import Nav from "../components/nav.svelte";
+
+</script>
+
+<div class="body">
+  <Nav/>
+  <div class="side_view">
+    <h1>Dubai <br/>Computing Community</h1>
+    <span></span>
+    <p>
+        As Computer Science exapnds across the UAE, we aim to foster a community where<br/>
+        programmers can connect, collaborate, and innovate regardless of age and skillset.
+    </p>
+    
+
+    <span class="Mobile_p">
+        As Computer Science exapnds across the UAE, we aim to foster a community where<br/>
+        programmers can connect, collaborate, and innovate regardless of age and skillset.
+    </span>
+    <div class="btn_ctn">
+        <span>
+            <Buttons title="Join our Community"/>
+        </span>
+        <span>
+            <Buttons title="See more about us"/>
+        </span>
+    </div>
+  </div>
+  <div>
+      <img src="/blob.svg" alt="blobl" class="blob">
+      <img src="/discord.svg" alt="discord" class="dsc"/>
+  </div>
+</div>
+
+
+<style>
+
+    .body{
+        height: 100vh;
+    }
+
+    .Mobile_p{
+        display: none;
+    }
+
+    .btn_ctn{
+        margin-top: 5%;
+        display: flex;
+        flex-direction: row;
+    }
+
+    .btn_ctn span{
+        margin-right: 5%;
+    }
+
+    .side_view{
+        margin-top: 5%;
+        margin-left: 5%;
+    }
+
+
+    .side_view h1{
+        font-size: 4rem;
+        font-family: var(--font_M);
+        font-weight:bolder;
+        margin-bottom: 1%;
+    }
+
+    .side_view p{
+        font-size: 20px;
+        color: grey;
+        font-family: var(--font_M);
+    }
+
+ .blob{
+    position: absolute;
+    right: -50%;
+    top: -100%;
+    width: 100rem;
+    z-index: -1;
+ }
+
+ .dsc{
+    width: 600px;
+    height: 400px;
+    position: absolute;
+    right: 5%;
+    top:15%;
+ }
+
+ 
+ @media only screen and (max-width: 600px) {
+     .btn_ctn, .side_view p{
+        display: none;
+    }
+
+    .blob{
+        right: -50%;
+        left: -100%;
+        width: 45rem;
+        height: 120rem;
+    }
+
+
+    .side_view h1{
+        margin-top: 10%;
+        color: #25E5A7;
+        font-size: 2.5rem;
+        font-style:italic;
+        font-weight: bolder;
+        line-height: 6rem;
+        display: inline-block;
+        max-width: min-content;
+    }
+
+    .side_view h1 br{
+        display: none;
+    }
+
+
+    .Mobile_p{
+        display: flex;
+        position: absolute;
+        top: 100vh;
+    }
+
+    .dsc{
+        width: 350px;
+        height: 350px;
+        margin-right: -6%;
+        margin-top: 50vh;
+    }
+
+ }
+
+</style>
